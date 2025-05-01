e2093a7b91179a0f0b2eb03981034a465afacb07
exiaquan <transient.exia@gmail.com>
2025-05-01 09:45:27 +0700

    logout

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,6 +12,6 @@ class AdminController extends Controller
         Auth::guard('web')->logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect('/');
+        return redirect('/login');
     }
 }
