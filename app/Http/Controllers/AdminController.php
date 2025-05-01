127ffc1bcc1030a78a7af1ffa9381f681d6a8816
exiaquan <transient.exia@gmail.com>
2025-05-01 09:41:44 +0700

    logout

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+
+class AdminController extends Controller
+{
+    public function AdminDestroy(Request $request): RedirectResponse {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+    }
+}
